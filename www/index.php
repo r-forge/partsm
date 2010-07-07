19b86f2a46163226b86a64b030289f48509182f9
cjgb (Mr ) <cgb@datanalytics.com>
2010-07-07 00:20:48 +0000

    Changed webpage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partsm@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,51 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
+	<title>R Package partsm</title>
+	<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
+</head>
 
 <body>
 
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="/"><img src="http://r-forge.r-project.org/themes/rforge/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+<h2>partsm R package</h2>
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<p>This package performs basic functions to fit and predict periodic autoregressive time series models as discussed in the book <a href="http://books.google.es/books?id=IJmMhRV93WsC">"Periodicity and Stochastic Trends in Economic Time Series"</a> by Philip Hans Franses. Data sets analyzed in that book are also provided.</p>
 
-<!-- end of project description -->
+<p>You can find it in <a href="http://cran.r-project.org/web/packages/partsm/index.html">CRAN</a> and can be installed using R's install.package function.</p>
 
-<p> No content added. </p>
+<p>The project summary page can be found <a href="http://r-forge.r-project.org/projects/partsm/">here</a>. You may also be interested in its page at <a href="http://crantastic.org/packages/partsm">crantastic</a>.</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
-
+<p>The package was originally developed by Javier López-de-Lacalle and it is currently mantained by <a href="http://www.datanalytics.com">Carlos J. Gil Bellosta</a>.</p>
 </body>
 </html>
+
